8fc570ac1dc88dfd276345b1822f4f743b406b4c
Salvador Mendo <simiographics@gmail.com>
2014-12-02 14:05:15 -0600

    Error en script en el link de skip content

--- assets/js/skip-content.js
+++ assets/js/skip-content.js
@@ -1,7 +1,6 @@
 $(function() {
   $("a[href^='#']").click(function() {
-    var destination = $(this).attr("href").slice(1);
-
-    $("#" + destination ).focus().effect("highlight", {}, 3000);
+    var destination = $(this).attr("href");
+    $(destination).focus();
   });
 });
