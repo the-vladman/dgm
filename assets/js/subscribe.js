7ec8db07bce2058d49bee2db1d2761714a54275c
Salvador Mendo <simiographics@gmail.com>
2014-12-03 19:26:33 -0600

    VO lee mal la sección de subscribe. #112

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -9,6 +9,7 @@ $(function() {
 
     if ( email.val() == "" ) {
       message.addClass('active error').text('Por favor ingresa algo en el campo.');
+      email.focus();
     } else {
       sendForm(form, message, email);
     }
@@ -24,10 +25,12 @@ $(function() {
         contentType: "application/json; charset=utf-8",
         error: function(err) {
           message.addClass('active error').text('Uups, algo paso que no pudimos registrate, intenta más tarde.')
+          emailField.focus();
         },
         success: function(data) {
           if (data.result != "success") {
             message.addClass('active error').text('Al parecer el correo electrónico que ingresaste no existe o es invalido.');
+            emailField.focus();
           } else {
             message.addClass('active success').text('Para completar tu registro verifica tu correo electrónico.');
             emailField.val("");
