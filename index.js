3b859c18dd65bd6d567d3c796a6490a002212cf2
Carlos Cessa <carlos@pixative.com>
2016-05-03 02:12:07 -0500

    Basic Layout
    - Added application basic layout
    - Created startup library to setup the url parameters retrieval and
    logging functionality
    - Set view engine and defined base frontend router and layouts

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express     = require( 'express' ),
     debug       = require( 'debug' )( 'app' ),
-    app         = express();
+    app         = express(),
+    start       = require( './lib/start' );
 
 // Set the PORT and ENV variables and start the server
 app.set( 'port', process.env.PORT || 3000 );
 app.set( 'env', process.env.ENV || 'development' );
 
-var server  = app.listen( app.get('port'), function() {
+start.launch( app );
+
+// Set the CMS server routes
+var front       = require( './routers/front' );
+
+app.use( '/', front );
+
+var server      = app.listen( app.get('port'), function() {
     debug( 'Express server listening on port ' + server.address().port );
 });
 
