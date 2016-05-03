322da5d7d4cef1102229bbf06a80368c243d5b1d
Carlos Cessa <carlos@pixative.com>
2016-05-03 02:48:36 -0500

    UI Router
    - Included UI router to the AngularJS application

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,13 @@
 'use strict';
 
 define( function () {
-    var app     = angular.module( 'dgm', [] );
+    var app     = angular.module( 'dgm', [
+            'ui.router'
+        ]);
+
+    app.config( [ '$locationProvider', function ( $locationProvider ) {
+        $locationProvider.html5Mode( true );
+    }]);
 
     return app;
 });
