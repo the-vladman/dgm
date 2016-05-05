36f3c97410c0838a38e38d0b3b69c1222c48c96c
Carlos Cessa <carlos@pixative.com>
2016-05-05 01:16:06 -0500

    Main Menu
    - Modified application admin main menu to set the user name as the
    user menu item

--- public/js/admin/AdminController.js
+++ public/js/admin/AdminController.js
@@ -1,7 +1,9 @@
 'use strict';
 
 define( function () {
-    return function () {
+    return function ( $rootScope, Sessions ) {
         $( 'body' ).addClass( 'admin' );
+
+        $rootScope.user     = Sessions.getUser();
     };
 });
