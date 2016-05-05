08e47f123a39101ec23db49f7e352d94d5c2908e
Carlos Cessa <carlos@pixative.com>
2016-05-04 19:11:01 -0500

    Utils Library
    - Created utils library to perform common tasks for the application
    - Added support to paginate data for the different server resources

--- config/app.example.js
+++ config/app.example.js
@@ -1,3 +1,4 @@
 module.exports  = {
-    encryption_key      : 'APPLICATION_ENCRYPTION_KEY_32_CHARACTERS'
+    encryption_key      : 'APPLICATION_ENCRYPTION_KEY_32_CHARACTERS',
+    page_size           : DEFAULT_PAGINATION_PAGE_SIZE
 };
