8a3d505f64aa342754b07ba65b62e02319d7b5ee
Carlos Cessa <carlos@pixative.com>
2016-05-05 18:59:43 -0500

    Landing State
    - Modified application front router to set the default state as
    landing and set the `home` state with the navbar and footer as the
    state to inherit for the frontpage views

--- public/js/front/FrontRouter.js
+++ public/js/front/FrontRouter.js
@@ -3,12 +3,21 @@
 define( function () {
     return function ( $stateProvider ) {
         $stateProvider
-            .state( 'home', {
-                url     : '/',
-                views   : {
+            .state( 'front', {
+                abstract    : true,
+                views       : {
                     application     : {
-                        templateUrl : 'partials/front/home.jade',
+                        templateUrl : 'partials/front/home',
                         controller  : 'FrontCtrl'
+                    }
+                }
+            })
+            .state( 'landing', {
+                parent      : 'front',
+                url         : '/',
+                views       : {
+                    'front-container'   : {
+                        templateUrl     : 'partials/front/landing'
                     }
                 }
             });
