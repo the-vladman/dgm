fc3e59866b3702175c8ac2bab4b57fca49547380
Carlos Cessa <carlos@pixative.com>
2016-05-05 03:26:26 -0500

    Users List
    - Added support to load a the existing users in the administration
    view

--- public/js/users/UsersAdminCtrl.js
+++ public/js/users/UsersAdminCtrl.js
@@ -2,6 +2,14 @@
 
 define( function () {
     return function ( $scope, Users ) {
-        
+        $scope.page     = 1;
+        $scope.query    = function () {
+            $scope.users    = Users.query({
+                page        : $scope.page,
+                per_page    : 10
+            });
+        };
+
+        $scope.query();
     };
 });
