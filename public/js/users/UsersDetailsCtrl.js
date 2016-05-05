d3944fc8681c79af67969a9d66834747c0ec1742
Carlos Cessa <carlos@pixative.com>
2016-05-05 15:32:48 -0500

    Users Details View
    - Created state to display the user details on screen
    - Included styles and template for the new application state
    - Added support to remove an user record from the database

--- public/js/users/UsersDetailsCtrl.js
+++ public/js/users/UsersDetailsCtrl.js
@@ -2,6 +2,23 @@
 
 define( function () {
     return function ( $scope, $stateParams, Users ) {
+        $scope.current  = Users.get( $stateParams.id );
 
+        $scope.save     = function () {
+            Users.update( $stateParams.id, $scope.current );
+        };
+
+        $scope.$on( Users.getEvent( 'DELETED' ), function () {
+            $scope.$state.go( 'users.list' );
+        });
+        $scope.$on( Users.getEvent( 'UPDATED' ), function () {
+            $scope.$state.go( 'users.list' );
+        });
+        $scope.$on( 'REMOVE_USER', function () {
+            Users.remove( $stateParams.id );
+        });
+        $scope.$on( 'UPDATE_USER', function () {
+            $scope.save();
+        });
     };
 });
