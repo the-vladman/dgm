6a36e284ef11c03a9a67a0dcd0d480fefcacb8a1
Carlos Cessa <carlos@pixative.com>
2016-05-11 01:57:19 -0500

    Server Multipart Requests
    - Added support to handle multipart requests for files uploading

--- config/app.example.js
+++ config/app.example.js
@@ -1,4 +1,5 @@
 module.exports  = {
     encryption_key      : 'APPLICATION_ENCRYPTION_KEY_32_CHARACTERS',
-    page_size           : DEFAULT_PAGINATION_PAGE_SIZE
+    page_size           : DEFAULT_PAGINATION_PAGE_SIZE,
+    uploads_tmp_path    : 'TEMPORARY_UPLOADS_PATH'
 };
