5ac859cf423800d412d98aabeddffa56c1720a0b
Carlos Cessa <carlos@pixative.com>
2016-05-11 23:33:30 -0500

    Categories Pagination
    - Added support to paginate the results in the application categories
    table

--- public/js/categories/CategoriesListCtrl.js
+++ public/js/categories/CategoriesListCtrl.js
@@ -3,14 +3,19 @@
 define( function () {
     return function ( $scope, Categories ) {
         $scope.page     = 1;
+        $scope.per_page = 10;
         $scope.query    = function () {
             $scope.categories   = Categories.query({
                 expanded        : true,
                 page            : $scope.page,
-                per_page        : 10,
+                per_page        : $scope.per_page,
                 sort            : 'type'
             });
         };
+
+        $scope.$on( Categories.getEvent( 'QUERIED' ), function () {
+            $scope.total    = Categories.getTotal();
+        });
 
         $scope.query();
     };
