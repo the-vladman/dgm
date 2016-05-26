d312ead7d067429d45f0301d200325b26bfadd6c
Carlos Cessa <carlos@pixative.com>
2016-05-26 15:11:45 -0500

    Category View
    - Added support to display a `tag` category page in the application's
    front end

---
+++ public/js/categories/CategoriesContentCtrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+define( function () {
+    return function ( $scope, $stateParams, Categories ) {
+        $scope.category     = Categories.get( $stateParams.category, true );
+    };
+});
