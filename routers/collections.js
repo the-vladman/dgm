054e2253072be2c1334e6847ae9d02cd67958710
Carlos Cessa <carlos@pixative.com>
2016-06-03 00:38:56 -0500

    Posts Export
    - Added support to export database posts to the generated zip file

--- routers/collections.js
+++ routers/collections.js
@@ -1,24 +1,23 @@
-var express             = require( 'express' ),
+var async               = require( 'async' ),
+    express             = require( 'express' ),
     fs                  = require( 'fs' ),
     jsonfile            = require( 'jsonfile' ),
     path                = require( 'path' ),
     config              = require( '../config/app' ),
     Category            = require( '../models/category' ),
+    Post                = require( '../models/post' ),
     nodeZip             = require( 'node-zip' ),
     router              = express.Router(),
     compressCategories  = function ( req, zip, cb ) {
         Category.find( function ( err, categories ) {
             if ( err || !categories ) {
-                err         = new Error( 'Invalid query' );
-                err.status  = 401;
-
-                cb( err );
+                cb( true );
             } else {
                 var categoriesFile  = path.join( config.uploads_tmp_path, 'categories.json' );
 
                 jsonfile.writeFile( categoriesFile, categories, {
                     spaces  : 2
-                }, function ( err ) {
+                }, function () {
                     zip.file( 'data/categories.json', fs.readFileSync( categoriesFile ) );
 
                     for ( var i = 0; i < categories.length; i++ ) {
@@ -37,14 +36,63 @@ var express             = require( 'express' ),
                 });
             }
         });
+    },
+    compressPosts       = function ( req, zip, cb ) {
+        Post.find( function ( err, posts ) {
+            if ( err || !posts ) {
+                cb( true );
+            } else {
+                var postsFile   = path.join( config.uploads_tmp_path, 'posts.json' );
+
+                jsonfile.writeFile( postsFile, posts, {
+                    spaces  : 2
+                }, function () {
+                    zip.file( 'data/posts.json', fs.readFileSync( postsFile ) );
+
+                    for ( var i = 0; i < posts.length; i++ ) {
+                        if ( posts[i].cover_photo && posts[i].cover_photo.path ) {
+                            zip.file( path.join( 'data/images/', posts[i].id, posts[i].cover_photo.name ), fs.readFileSync( posts[i].cover_photo.path ) );
+                        }
+
+                        if ( posts[i].grid_photo && posts[i].grid_photo.path ) {
+                            zip.file( path.join( 'data/images/', posts[i].id, posts[i].grid_photo.name ), fs.readFileSync( posts[i].grid_photo.path ) );
+                        }
+                    }
+
+                    cb( null, zip );
+                });
+            }
+        });
     };
 
 router.get( '/', function ( req, res, next ) {
     var zip     = new nodeZip();
 
-    compressCategories( req, zip, function ( err, zip ) {
+    async.waterfall([
+        function ( cb ) {
+            compressCategories( req, zip, function ( err, zip ) {
+                if ( err ) {
+                    cb( true );
+                } else {
+                    cb( null, zip );
+                }
+            });
+        },
+        function ( zip, cb ) {
+            compressPosts( req, zip, function ( err, zip ) {
+                if ( err ) {
+                    cb( true );
+                } else {
+                    cb( null, zip );
+                }
+            });
+        }
+    ], function ( err, zip ) {
         if ( err ) {
-            next( err );
+            err         = new Error( 'Invalid query' );
+            err.status  = 401;
+
+            cb( err );
         } else {
             var zipFile     = path.join( config.uploads_tmp_path, 'data.zip' );
 
