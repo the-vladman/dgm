315dfeb678efb6e6db4b41e2a2ee4cf65641ec76
Carlos Cessa <carlos@pixative.com>
2016-06-06 16:07:11 -0500

    Import Support
    - Added support to import database information from a generated zip
    file

--- routers/collections.js
+++ routers/collections.js
@@ -3,6 +3,7 @@ var async               = require( 'async' ),
     fs                  = require( 'fs' ),
     jsonfile            = require( 'jsonfile' ),
     path                = require( 'path' ),
+    mkdirp              = require( 'mkdirp' ),
     config              = require( '../config/app' ),
     Category            = require( '../models/category' ),
     Post                = require( '../models/post' ),
@@ -81,6 +82,204 @@ var async               = require( 'async' ),
                 });
             }
         });
+    },
+    loadCategories      = function ( req, zip, cb ) {
+        var categories  = JSON.parse( zip.files[ 'data/categories.json' ]._data ),
+            ids         = Array(),
+            photos      = Array();
+
+        for ( var i = 0; i < categories.length; i++ ) {
+            ids.push({
+                original    : categories[i]._id
+            });
+
+            photos.push({
+                cover_photo : categories[i].cover_photo,
+                grid_photo  : categories[i].grid_photo
+            });
+
+            delete categories[i]._id;
+            delete categories[i].cover_photo;
+            delete categories[i].grid_photo;
+        }
+
+        Category.create( categories, function ( err, records ) {
+            if ( err ) {
+                err         = new Error( 'Invalid categories data' );
+                err.status  = 403;
+
+                cb( err );
+            } else {
+                for ( var i = 0; i < records.length; i++ ) {
+                    ids[i].created  = records[i].id;
+                }
+
+                var i = 0;
+                async.each( records, function ( category, done ) {
+                    if ( category.type == 'TAG' ) {
+                        var dir     = path.join( config.uploads_path, category.id );
+                        mkdirp.sync( dir );
+
+                        if ( photos[i].cover_photo != undefined ) {
+                            fs.writeFileSync( path.join( dir, photos[i].cover_photo.name ), zip.files['data/images/' + ids[i].original + '/' + photos[i].cover_photo.name]._data, 'binary' );
+                            category.cover_photo    = {
+                                path    : path.join( dir, photos[i].cover_photo.name ),
+                                name    : photos[i].cover_photo.name
+                            };
+                        }
+
+                        if ( photos[i].grid_photo != undefined ) {
+                            fs.writeFileSync( path.join( dir, photos[i].grid_photo.name ), zip.files['data/images/' + ids[i].original + '/' + photos[i].grid_photo.name]._data, 'binary' );
+                            category.grid_photo     = {
+                                path    : path.join( dir, photos[i].grid_photo.name ),
+                                name    : photos[i].grid_photo.name
+                            };
+                        }
+
+                        category.save( function () {
+                            i++;
+                            done( null );
+                        });
+                    } else {
+                        i++;
+                        done( null );
+                    }
+                }, function ( err ) {
+                    cb( null, ids );
+                });
+            }
+        });
+    },
+    loadPosts           = function ( req, zip, usersIds, categoriesIds, cb ) {
+        var posts       = JSON.parse( zip.files[ 'data/posts.json' ]._data ),
+            ids         = Array(),
+            photos      = Array();
+
+        for ( var i = 0; i < posts.length; i++ ) {
+            var author      = false,
+                category    = false,
+                editor      = false,
+                publisher   = false,
+                section     = false,
+                tag         = false;
+
+            ids.push( posts[i]._id );
+            photos.push({
+                cover_photo : posts[i].cover_photo,
+                grid_photo  : posts[i].grid_photo
+            });
+
+            delete posts[i]._id;
+            delete posts[i].cover_photo;
+            delete posts[i].grid_photo;
+
+            for ( var j = 0; j < usersIds.length; j++ ) {
+                if ( !author && ( posts[i].created_by && posts[i].created_by == usersIds[j].original ) ) {
+                    posts[i].created_by     = usersIds[j].created;
+                    author                  = true;
+                }
+
+                if ( !editor && ( posts[i].edited_by && posts[i].edited_by == usersIds[j].original ) ) {
+                    posts[i].edited_by      = usersIds[j].created;
+                    editor                  = true;
+                }
+
+                if ( !publisher && ( posts[i].published_by && posts[i].published_by == usersIds[j].original ) ) {
+                    posts[i].published_by   = usersIds[j].created;
+                    publisher               = true;
+                }
+
+                if ( author && editor && publisher ) {
+                    break;
+                }
+            }
+
+            for ( var j = 0; j < categoriesIds.length; j++ ) {
+                if ( !category && posts[i].category == categoriesIds[j].original ) {
+                    posts[i].category   = categoriesIds[j].created;
+                    category            = true;
+                }
+
+                if ( !section && posts[i].section == categoriesIds[j].original ) {
+                    posts[i].section    = categoriesIds[j].created;
+                    section             = true;
+                }
+
+                if ( !tag && posts[i].tag == categoriesIds[j].original ) {
+                    posts[i].tag        = categoriesIds[j].created;
+                    tag                 = true;
+                }
+
+                if ( category && section && tag ) {
+                    break;
+                }
+            }
+        }
+
+        Post.create( posts, function ( err, records ) {
+            if ( err ) {
+                err         = new Error( 'Invalid posts data' );
+                err.status  = 403;
+
+                cb( err );
+            } else {
+                var i = 0;
+                async.each( records, function ( post, done ) {
+                    var dir         = path.join( config.uploads_path, post.id );
+
+                    mkdirp.sync( dir );
+
+                    if ( photos[i].cover_photo != undefined ) {
+                        fs.writeFileSync( path.join( dir, photos[i].cover_photo.name ), zip.files['data/images/' + ids[i] + '/' + photos[i].cover_photo.name]._data, 'binary' );
+                        post.cover_photo    = {
+                            path    : path.join( dir, photos[i].cover_photo.name ),
+                            name    : photos[i].cover_photo.name
+                        };
+                    }
+
+                    if ( photos[i].grid_photo != undefined ) {
+                        fs.writeFileSync( path.join( dir, photos[i].grid_photo.name ), zip.files['data/images/' + ids[i] + '/' + photos[i].grid_photo.name]._data, 'binary' );
+                        post.grid_photo     = {
+                            path    : path.join( dir, photos[i].grid_photo.name ),
+                            name    : photos[i].grid_photo.name
+                        };
+                    }
+
+                    post.save( function () {
+                        i++;
+                        done( null );
+                    });
+                }, function ( err ) {
+                    cb( null );
+                });
+            }
+        });
+    },
+    loadUsers           = function ( req, zip, cb ) {
+        var users       = JSON.parse( zip.files[ 'data/users.json' ]._data ),
+            ids         = Array();
+
+        for ( var i = 0; i < users.length; i++ ) {
+            ids.push({
+                original    : users[i]._id
+            });
+            delete users[i]._id;
+        }
+
+        User.create( users, function ( err, records ) {
+            if ( err ) {
+                err         = new Error( 'Invalid users data' );
+                err.status  = 403;
+
+                cb( err );
+            } else {
+                for ( var i = 0; i < records.length; i++ ) {
+                    ids[i].created  = records[i].id;
+                }
+
+                cb( null, ids );
+            }
+        });
     };
 
 router.get( '/', function ( req, res, next ) {
@@ -136,6 +335,59 @@ router.get( '/', function ( req, res, next ) {
             filestream.pipe( res );
         }
     });
+});
+
+router.post( '/', function ( req, res, next ) {
+    if ( !req.uploading || !req.body.files.data ) {
+        var err     = new Error( 'Invalid request' );
+        err.status  = 400;
+
+        next( err );
+    } else {
+        var zipFile = fs.readFileSync( req.body.files.data[0].path ),
+            zip     = new nodeZip( zipFile, {
+                base64      : false,
+                checkCRC32  : true
+            });
+
+        async.waterfall([
+            function ( cb ) {
+                loadUsers( req, zip, function ( err, usersIds ) {
+                    if ( err ) {
+                        cb( err );
+                    } else {
+                        cb( null, usersIds );
+                    }
+                });
+            },
+            function ( usersIds, cb ) {
+                loadCategories( req, zip, function ( err, categoriesIds ) {
+                    if ( err ) {
+                        cb( err );
+                    } else {
+                        cb( null, usersIds, categoriesIds );
+                    }
+                });
+            },
+            function ( usersIds, categoriesIds, cb ) {
+                loadPosts( req, zip, usersIds, categoriesIds, function ( err ) {
+                    if ( err ) {
+                        cb( err );
+                    } else {
+                        cb();
+                    }
+                });
+            }
+        ], function ( err, result ) {
+            if ( err ) {
+                next( err );
+            } else {
+                res.json({
+                    message : 'Import complete'
+                });
+            }
+        });
+    }
 });
 
 module.exports  = router;
