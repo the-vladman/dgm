e443ab46ebe5382679c5095c4018419947036c94
Carlos Cessa <carlos@pixative.com>
2016-06-06 19:31:11 -0500

    Bug Fix
    - Fixed bug that prevented the `docker run` command from generating
    the `app.js` configuration file correctly

--- commands/replace_env.sh
+++ commands/replace_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    key=$( echo $line | grep -oP '([A-Z0-9_]*)' )
-    replaced=$( echo $line | sed "s/{{[A-Z0-9_]*}}/${!key}/g" )
+    key=$( echo $line | grep -oP '(?<={{)[^}}]*' )
+    replaced=$( echo $line | sed "s%{{[A-Z0-9_]*}}%${!key}%g" )
     echo "$replaced" >> "$2"
 done < "$1"
