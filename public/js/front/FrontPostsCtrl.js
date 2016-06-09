b64064d41cea88aaa692eac2834c80b41cd54f91
Carlos Cessa <carlos@pixative.com>
2016-06-09 17:47:25 -0500

    Landing Posts
    - Modified landing posts controller to display a featured post at the
    top of each section

--- public/js/front/FrontPostsCtrl.js
+++ public/js/front/FrontPostsCtrl.js
@@ -2,9 +2,20 @@
 
 define( function () {
     return function ( $scope, $element, $stateParams, Categories, Posts ) {
-        $scope.section  = $element.data( 'section' );
-        var per_page    = 4,
-            queryPosts  = function ( section, tag ) {
+        $scope.section      = $element.data( 'section' );
+        var per_page        = 4,
+            queryFeatured   = function ( section, tag ) {
+                $scope.featured = Posts.query({
+                    expanded    : true,
+                    featured    : true,
+                    page        : 1,
+                    per_page    : 1,
+                    section     : section,
+                    status      : 'PUBLISHED',
+                    tag         : tag
+                });
+            },
+            queryPosts      = function ( section, tag ) {
                 $scope.posts    = Posts.query({
                     expanded    : true,
                     featured    : false,
@@ -15,7 +26,7 @@ define( function () {
                     tag         : tag
                 });
             },
-            queryTag    = function () {
+            queryTag        = function () {
                 Categories.query({
                     page        : 1,
                     per_page    : 1,
@@ -23,9 +34,12 @@ define( function () {
                     slug        : $stateParams.category,
                     type        : 'TAG'
                 }).$promise.then( function ( data ) {
-                    if ( data[0] ) {
-                        queryPosts( $scope.section_id, data[0]._id );
+                    if ( !data[0] ) {
+                        return;
                     }
+
+                    queryFeatured( $scope.section_id, data[0]._id );
+                    queryPosts( $scope.section_id, data[0]._id );
                 });
             };
 
@@ -36,6 +50,10 @@ define( function () {
             slug        : $element.data( 'section' ),
             type        : 'SECTION'
         }).$promise.then( function ( data ) {
+            if ( !data[0] ) {
+                return;
+            }
+
             $scope.section_id   = data[0]._id;
             if ( data.length == 1 ) {
                 if ( $stateParams.category ) {
@@ -43,6 +61,7 @@ define( function () {
                     per_page    = 3;
                     queryTag();
                 } else {
+                    queryFeatured( $scope.section_id );
                     queryPosts( $scope.section_id );
                 }
             }
