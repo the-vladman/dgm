851a200c2c2073326b704839a58da312d9d8a245
Carlos Cessa <carlos@pixative.com>
2016-07-22 12:11:13 -0500

    Datasets Service
    - Created service to interact with the API's `datasets` resource

--- public/js/datasets/DatasetsModule.js
+++ public/js/datasets/DatasetsModule.js
@@ -2,8 +2,11 @@
 
 define( function ( require ) {
     var DatasetsRouter      = require( 'datasets/DatasetsRouter' );
+    var DatasetsService     = require( 'datasets/DatasetsService' );
 
     var DatasetsModule      = angular.module( 'DatasetsModule', []);
 
     DatasetsModule.config([ '$stateProvider', DatasetsRouter ]);
+
+    DatasetsModule.factory( 'DatasetsService', [ 'BaseService', DatasetsService ]);
 });
