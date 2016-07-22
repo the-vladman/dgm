e5aa2350c417b52959a21f1ad3af3b3bbcdaff60
Carlos Cessa <carlos@pixative.com>
2016-07-22 16:37:33 -0500

    Datasets Removal
    - Added support to remove datasets records from the system

--- public/js/datasets/DatasetsEditCtrl.js
+++ public/js/datasets/DatasetsEditCtrl.js
@@ -8,11 +8,17 @@ define( function () {
             Datasets.update( $stateParams.id, $scope.dataset );
         };
 
+        $scope.$on( Datasets.getEvent( 'DELETED' ), function () {
+            $scope.$state.go( 'datasets.list' );
+        });
         $scope.$on( Datasets.getEvent( 'UPDATED' ), function () {
             $scope.$state.go( 'datasets.list' );
         });
         $scope.$on( 'UPDATE_DATASET', function () {
             $scope.create();
+        });
+        $scope.$on( 'REMOVE_DATASET', function () {
+            Datasets.remove( $stateParams.id );
         });
     };
 });
