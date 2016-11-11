d6786554d35ab362276b845af864dabf61e237bb
KielRodriguez <kr.rdz.20@gmail.com>
2016-11-11 17:20:14 -0600

    fix: method queryFeature, add lines deleted

--- public/js/front/FrontPostsCtrl.js
+++ public/js/front/FrontPostsCtrl.js
@@ -6,6 +6,8 @@ define( function () {
         var per_page        = ( $scope.section == 'blog' ) ? 4 : 6,
             queryFeatured   = function ( section, tag ) {
                 $scope.featured = Posts.query({
+                    expanded    : true,
+                    featured    : true,
                     page        : 1,
                     per_page    : 1,
                     section     : section,
@@ -73,7 +75,7 @@ define( function () {
         }).$promise.then( function ( data ){
             if( !data[0] )
                 return
-            $scope.visualizers = data;
+            $scope.visualizer = data[0];
         });
     };
 });
