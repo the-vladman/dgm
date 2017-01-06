e30d84ef3614a3ebaa12726c0fa42e86c57c7086
KielRodriguez <kr.rdz.20@gmail.com>
2017-01-05 18:49:06 -0600

    fix: Chage path ckan api to https

--- public/js/common/CkanService.js
+++ public/js/common/CkanService.js
@@ -15,7 +15,7 @@ define( function () {
 
             _total          : 0,
 
-            _resource       : $resource( 'http://datos.gob.mx/busca/api/3/action/:action', null, {
+            _resource       : $resource( 'https://datos.gob.mx/busca/api/3/action/:action', null, {
                 dataset                 : {
                     method              : 'GET',
                     isArray             : false,
