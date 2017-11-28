8404bc5fabba77949db5dad17f13703e97e7dd96
Leonel Castañeda <leodcastaneda@gmail.com>
2017-11-28 14:35:15 -0600

    redirection engine

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var express = require('express'),
   app = express(),
   livereload = require('livereload'),
   error = require('./lib/error'),
-  start = require('./lib/start');
+  start = require('./lib/start'),
+  request = require('request'),
+  schedule = require('node-schedule');
 
 // Set the PORT and ENV variables and start the server
 app.set('port', process.env.PORT || 3000);
@@ -11,6 +13,7 @@ app.set('env', process.env.ENV || 'development');
 app.settings.env = app.get('env');
 process.env.CDN_URL = process.env.CDN_URL || 'http://cdn.datos.gob.mx/bower_components/';
 
+buildDGMOrganizationsVar();
 start.launch(app);
 
 // Set the CMS server routes
@@ -32,3 +35,29 @@ var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
 });
 module.exports = app;
+
+/*
+  redirect engine env variable
+  From this env the function validates if is a valid organization
+*/
+function buildDGMOrganizationsVar(){
+  request.get("https://api.datos.gob.mx/v1/ckan-organizations?pageSize=9999", function(error, response, body){
+
+    if(error){
+      console.log("Error updating DGM_ORGANIZATIONS_REDIRECT_ENGINE");
+    }else{
+      body = JSON.parse(body);
+      if( body.results instanceof Array ){
+        var organizations = ",";
+        for(var organization of body.results){organizations += organization.name + ",";}
+        process.env.DGM_ORGANIZATIONS_REDIRECT_ENGINE=organizations;
+      }
+    }
+
+  });
+}
+
+// update the var each day
+schedule.scheduleJob('15 1 * * *', function(){
+  buildDGMOrganizationsVar();
+});
--- routers/front.js
+++ routers/front.js
@@ -1,6 +1,26 @@
 var express = require('express'),
   router = express.Router();
 
+/*
+  redirect engine
+*/
+router.all("/", function(req, res, next){
+  var originUrl = req.get("referer");
+
+  if(originUrl && originUrl.match(/.*gob.mx.*/gi)){
+    var match = originUrl.match(/.*www\.(\w*)?\.?(?:gob\.mx\/)(\w*)?/i);
+    var organization = match==null ? null:match[1] ? match[1]:match[2];
+    var regex = new RegExp("," + organization + ",", "i");
+
+    if(process.env.DGM_ORGANIZATIONS_REDIRECT_ENGINE.match(regex)){
+      res.redirect("busca/organization/" + organization);
+      return;
+    }
+  }
+
+  next();
+});
+
 router.get('/', function(req, res) {
   res.render('index');
 });
