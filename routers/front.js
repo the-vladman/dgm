81fab3f7ff740ef4d84684144897f7dae2f2664c
Leonel Castañeda <leodcastaneda@gmail.com>
2017-12-01 00:43:49 -0600

    set redirection engine variable as array

--- routers/front.js
+++ routers/front.js
@@ -1,18 +1,20 @@
-var express = require('express'),
+var request = require('request'),
+  schedule = require('node-schedule')
+  express = require('express'),
   router = express.Router();
 
 /*
   redirect engine
 */
+var organizations;
 router.all("/", function(req, res, next){
   var originUrl = req.get("referer");
 
   if(originUrl && originUrl.match(/.*gob.mx.*/gi)){
     var match = originUrl.match(/.*www\.(\w*)?\.?(?:gob\.mx\/)(\w*)?/i);
     var organization = match==null ? null:match[1] ? match[1]:match[2];
-    var regex = new RegExp("," + organization + ",", "i");
 
-    if(process.env.DGM_ORGANIZATIONS_REDIRECT_ENGINE.match(regex)){
+    if (organization && organizations.indexOf(organization) > -1) {
       res.redirect("busca/organization/" + organization);
       return;
     }
@@ -33,4 +35,26 @@ router.get('*', function(req, res) {
   res.render('index');
 });
 
+/*
+  redirect engine env variable
+  From this env the function validates if is a valid organization
+*/
+function buildDGMOrganizationsVar(){
+  request.get("https://api.datos.gob.mx/v1/ckan-organizations?pageSize=9999", function(error, response, body){
+
+    if(error){
+      console.log("Error updating DGM_ORGANIZATIONS_REDIRECT_ENGINE");
+    }else{
+      body = JSON.parse(body);
+      organizations = body.results instanceof Array ? body.results:organizations;
+    }
+  });
+}
+
+// update the var each day
+schedule.scheduleJob('15 1 * * *', function(){
+  buildDGMOrganizationsVar();
+});
+
+buildDGMOrganizationsVar();
 module.exports = router;
