98de243b6583eca6869242e31f0d0e55e4ba1f13
Edson Amaya <edsonjab@gmail.com>
2018-02-21 16:58:12 -0600

    Se elimina disqus

--- public/js/posts/PostsFrontCtrl.js
+++ public/js/posts/PostsFrontCtrl.js
@@ -21,30 +21,6 @@ define( function () {
                 }
             }
 
-            var disqusShortname         = 'datosgobmx',
-                config                  = function () {
-                    this.language           = 'es_MX';
-                    this.page.url           = location;
-                    this.page.identifier    = $scope.post.slug;
-                    this.page.title         = $scope.post.name;
-                    this.experiment.enable_scroll_container = false;
-                };
-
-            if ( window.DISQUS == undefined ) {
-                window.disqus_config    = config;
-                ( function () {
-                    var dsq     = document.createElement( 'script' );
-                    dsq.type    = 'text/javascript';
-                    dsq.async   = true;
-                    dsq.src     = '//' + disqusShortname + '.disqus.com/embed.js';
-                    ( document.getElementsByTagName( 'head' )[0] || document.getElementsByTagName( 'body' )[0]).appendChild( dsq );
-                })();
-            } else {
-                DISQUS.reset({
-                    reload  : true,
-                    config  : config
-                });
-            }
         });
     };
 });
