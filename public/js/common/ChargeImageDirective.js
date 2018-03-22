e82aebcc77f5fbdcbc94670061d7ff6e783bcafb
Giovanni Mendoza <mendozagioo@gmail.com>
2018-03-22 02:51:10 -0600

    Acualización de loading estándar para contenidos

--- public/js/common/ChargeImageDirective.js
+++ public/js/common/ChargeImageDirective.js
@@ -6,14 +6,10 @@ define(function () {
       restrict: 'A',
       link: function (scope, element, attrs) {
         element.bind('load', function () {
-          var loaders = angular.element(document.getElementsByClassName('preloaders'));
-          var catego = angular.element(document.getElementsByClassName('categories-list'));
-
-          loaders.addClass('hidden');
-          catego.addClass('show');
+          element.removeClass('loading-content');
         });
         element.bind('error', function () {
-          alert('image could not be loaded');
+          // alert('image could not be loaded');
         });
       }
     };
